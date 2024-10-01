ad973168d3249caab77a14bc32edbe66ad55bc42
jwporath <jacob_porath@outlook.com>
2024-10-01 12:15:41 -0700

    Added basic placeholder pages

---
+++ post.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome!</title>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, inital-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="sidebar">
+        <p>E-XChange</p>
+        <ul>
+            <li><a href="index.php">Bulletin Board</a></li>
+            <li><a href="post.php">Create a Post</a></li>
+            <li><a href="signup.php">Sign Up</a></li>
+            <li><a href="signin.php">Sign In</a></li>
+        </ul>
+    </div>
+    <div class="main">
+        
+    </div>
+</body>
+
+</html>
